aeb9ad3c08c2a54a8601e6f0ca65b04d84292e64
Ben <beniigle8@gmail.com>
2025-11-19 14:18:39 -0300

    arsound posible fIXXX

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
-    reactStrictMode: false
   },
   images: {
     unoptimized: true,
